8ecc58f4b1101c081dad5d026abe0dd27f4c445b
Brianna Jordan <bryvjordan9@gmail.com>
2019-10-14 17:19:49 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,8 +3,6 @@ require_relative './class_methods_module.rb'
 require_relative './fancy_dance.rb'
 
 class Dancer
-  # include Dance
-  # extend MetaDancing
   include FancyDance::InstanceMethods
   extend FancyDance::ClassMethods
   attr_accessor :name
